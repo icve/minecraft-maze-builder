f1932d6ad62d1fc6d3b448ad21cd84550afdafd1
G.Cai <ic.ve.cv@gmail.com>
2017-12-15 23:04:10 +1100

    added command builder

--- index.js
+++ index.js
@@ -5,27 +5,35 @@ const { buildGen } = require('./buildGen');
 const { Builder } = require('./builder.js');
 
 const bot = mineflayer.createBot({
-      host: "localhost", // optional
-      port: 43115,       // optional
-      username: "gg", // email and password are required only for
+    host: "localhost", // optional
+    port: 34851,       // optional
+    username: "gg", // email and password are required only for
 });
 
 bot.on('chat', function(username, message) {
-      if (username === bot.username) return;
-      bot.chat(message);
+    if (username === bot.username) return;
+    bot.chat(message);
 });
 
 const bder = new Builder(bot);
 
 
 bot.once('spawn', () =>{
+    //build();
 
+    bot.chat('/tp c');
+    setTimeout(command_builder, 1000);
+});
+
+/*
+ * normal build mode
+*/
+const build = () =>{
     const pos = bot.players.c.entity.position.clone();
-
     console.log('starting at', pos);
-     const bg = buildGen('./map1.txt', pos, 2);
+    const bg = buildGen('./map1.txt', pos, 2);
 
-      if(bder.free){
+    if(bder.free){
         bder.on('complete', ()=>{
             const nxt = bg.next()
             if(nxt.done){
@@ -37,14 +45,21 @@ bot.once('spawn', () =>{
 
         bder.build(bg.next().value);
     }
+}
 
-    });
+/*
+ * build using command. (bot need to be op)
+ */
+const command_builder = () =>{
 
+    const pos = bot.players.c.entity.position.offset(1, 0, 0);
+    console.log('starting at', pos);
+    const bg = buildGen('./map3.txt', pos, 5);
+    const block_type = 'minecraft:planks';
 
-const stepper = (n, it) => {
-    let val = it.next().value;
-    for(let i=0;i<n;i++){
-        val = it.next().value;
+    for(let obj of bg){
+       const tc = obj.tcoord;
+       const cmd = "/setblock " + tc.x + " " + tc.y + " " + tc.z + " " + block_type;
+       bot.chat(cmd);
     }
-    return val;
 }
